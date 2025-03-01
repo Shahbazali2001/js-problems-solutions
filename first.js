27a98a8c523fc743d58c12a2d568d8204bc7c0e1
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-01 21:54:30 +0500

    First Challenge Solved

---
+++ first.js
@@ -0,0 +1,15 @@
+const marksWeight = 78;
+const marksHeight = 1.69;
+const marksBMI = marksHeight/(marksHeight*marksHeight);
+
+console.log('Marks BMI:', marksBMI);
+
+const johnWeight = 68;
+const johnHeight =1.95;
+const johnBMI = johnHeight/(johnHeight*johnHeight);
+
+console.log('John BMI:', johnBMI);
+
+bool = marksBMI > johnBMI;
+
+console.log('Marks BMI is greater than John BMI:', bool);
