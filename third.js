3e0e00e303a9a4459e3e4bb321c4fa95e9b9d878
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-05 23:55:44 +0500

    For Loop Added

--- third.js
+++ third.js
@@ -1,28 +1,14 @@
-//Closure Function
-
-var createCounter = function(number){
+var createCounter = function (number) {
     let counter = number;
-
-    return function(){
+    
+    return function () {
         counter++;
         return counter;
-
-    }
-}
+    };
+};
 
 var counter1 = createCounter(0);
-console.log(counter1());
-console.log(counter1());
-console.log(counter1());
 
-var counter2 = createCounter(-5);
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
-console.log(counter2());
+for (var i = 0; i < 5; i++) { // Running closure function 5 times
+    console.log(counter1());
+}
