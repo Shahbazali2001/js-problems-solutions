8e289acac3f3165da04fdbb4fac8276fc8a5509b
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-17 23:15:08 +0500

    Bug Fixes

--- fourth.js
+++ fourth.js
@@ -1,1 +1,17 @@
 console.log("Hello this is fourth question")
+
+// Import express module
+const express = require('express');
+
+// Create a new express application
+ const app = express();
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Define a route for the about page
+ app.get('/about', (req, res) => {
+  res.send('About Page');
+});
